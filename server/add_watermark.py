a6f32d9fb501e164e505888819e569d078f0f6a1
Ian Korovinsky <ian.korovinsky@gmail.com>
2023-08-19 09:38:13 -0400

    watermark added to the pdf, prompts for GPT were engineered

---
+++ server/add_watermark.py
@@ -0,0 +1,32 @@
+from typing import Union, Literal, List
+
+from PyPDF2 import PdfWriter, PdfReader
+
+
+def watermark(
+    content_pdf = "server\\tmp\\business_plan.pdf",
+    stamp_pdf = "server\\watermark.pdf",
+    pdf_result = "server/tmp/modified_business_plan.pdf",
+    page_indices: Union[Literal["ALL"], List[int]] = "ALL",
+):
+    reader = PdfReader(content_pdf)
+    if page_indices == "ALL":
+        page_indices = list(range(0, len(reader.pages)))
+
+    writer = PdfWriter()
+    for index in page_indices:
+        content_page = reader.pages[index]
+        mediabox = content_page.mediabox
+
+        # You need to load it again, as the last time it was overwritten
+        reader_stamp = PdfReader(stamp_pdf)
+        image_page = reader_stamp.pages[0]
+
+        image_page.merge_page(content_page)
+        image_page.mediabox = mediabox
+        writer.add_page(image_page)
+
+    with open(pdf_result, "wb") as fp:
+        writer.write(fp)
+
+watermark()
