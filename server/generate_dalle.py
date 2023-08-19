457353e280d49eb807b17a56fef44c2f8e27665e
Brian Yan <todaybryan@gmail.com>
2023-08-19 10:03:45 -0400

    implement dall-e image downloading

--- server/generate_dalle.py
+++ server/generate_dalle.py
@@ -1,5 +1,8 @@
 import openai
 import os
+import requests
+import shutil
+from random import randint
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -8,8 +11,17 @@ generate_prompt = lambda query: f"{query} in vaporwave style"
 def generate_dalle(query):
     response = openai.Image.create(
         prompt=generate_prompt(query),
-        n=1,
-        size="512x512",
+        n=4,
+        size="256x256",
     )
-    image_url = response['data'][0]['url']
-    return image_url
+    file_names=[]
+    for i in range(0, len(response['data'])):
+        image_url = response['data'][i]['url']
+        image_res = requests.get(image_url, stream=True)
+        file_name = f"tmp/{randint(0, 1000000)}.png"
+        file_names.append(file_name)
+        with open(file_name, "wb") as out_file:
+            shutil.copyfileobj(image_res.raw, out_file)
+        del image_res
+        
+    return file_names   
