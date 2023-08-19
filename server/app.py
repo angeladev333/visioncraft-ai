01d8310fdc80bb6bd3800662f8cc1840897f7320
Ian Korovinsky <ian.korovinsky@gmail.com>
2023-08-19 00:05:22 -0400

    test version of darknet implemented

--- server/app.py
+++ server/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+
 
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    data ={
+        'Type':'Person',
+        'Age':18
+    }
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
