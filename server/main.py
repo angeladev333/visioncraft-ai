abc2a24e49df9a36055e09040daea99a92b54da9
Brian Yan <todaybryan@gmail.com>
2023-08-19 16:04:31 -0400

    add changes for google cloud app engine

--- server/main.py
+++ server/main.py
@@ -95,4 +95,5 @@ def upload_to_ipfs():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port="8080") 
+    # app.run(debug=True)
+    app.run('127.0.0.1', port=8080, debug=True)
