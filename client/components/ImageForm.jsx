819f99deb5bd5a7653d2a7c8221fb5883ce646b6
Lucy Qi <l8qi@uwaterloo.ca>
2023-08-20 00:40:55 -0400

    removing dark mode

--- client/components/ImageForm.jsx
+++ client/components/ImageForm.jsx
@@ -33,7 +33,7 @@ export default function ImageForm() {
         <div className="mb-6">
           <label
             htmlFor="whatcreate"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900"
           >
             What do you want to create?
           </label>
@@ -41,7 +41,7 @@ export default function ImageForm() {
             type="text"
             name="whatcreate"
             id="whatcreate"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="A Mini PC"
             required
           />
@@ -49,7 +49,7 @@ export default function ImageForm() {
         <div className="mb-6">
           <label
             htmlFor="materials"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900"
           >
             What materials do you have?
           </label>
@@ -57,7 +57,7 @@ export default function ImageForm() {
             type="text"
             name="materials"
             id="materials"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
             placeholder="Power Adapter, Motherboard, ..."
             required
           />
@@ -65,7 +65,7 @@ export default function ImageForm() {
         <div className="mb-6">
           <label
             htmlFor="budget"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-gray-900"
           >
             What is your budget?
           </label>
@@ -73,21 +73,21 @@ export default function ImageForm() {
             type="number"
             name="budget"
             id="budget"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="2000"
             required
           />
         </div>
 
         <label
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          className="block mb-2 text-sm font-medium text-gray-900"
           htmlFor="user_avatar"
         >
           Upload Image of Hardware Parts.
         </label>
         <input
           name="image"
-          className="mb-6 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          className="mb-6 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50"
           aria-describedby="user_avatar_help"
           id="user_avatar"
           type="file"
@@ -95,7 +95,7 @@ export default function ImageForm() {
 
         <button
           type="submit"
-          className="rounded-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium w-48 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="rounded-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium w-48 text-sm px-5 py-2.5 mr-2 mb-2"
         >
           Submit
         </button>
