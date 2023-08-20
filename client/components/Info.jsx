9d6f6ecd5ae6985e649513bf272c3279e91a9b87
Lucy Qi <l8qi@uwaterloo.ca>
2023-08-19 20:58:15 -0400

    updated front page

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -5,30 +5,29 @@ import Image from "next/image";
 export default function Info() {
   const list = [
     {
-      title: "Identify",
       img: "/image1.png",
-      description: "Identify",
     },
     {
-      title: "Identify",
       img: "/image2.png",
-      description: "Identify",
     },
     {
-      title: "Identify",
       img: "/image3.png",
-      description: "Identify",
     },
   ];
+
+
+
   return (
     <div className="flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       <div className="text-center z-10">
-        <h2 className="text-2xl font-bold p-6 text-sky-500">Identify</h2>
+      <div className="bg-white p-4 rounded shadow-md rounded-3xl">
+        <h2 className="text-4xl font-bold p-6 text-violet-500">BUILD</h2>
 
         <h1 className="text-5xl w-2/3 break-normal mx-auto mb-10 p-6 font-extrabold">
-          Upload any image and we find what you can use
+          Upload any image and we'll find what you can create
         </h1>
-      </div>
+      
+      
       <div className="gap-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {list.map((item, index) => (
           <Card
@@ -37,6 +36,8 @@ export default function Info() {
             isPressable
             onPress={() => console.log("item pressed")}
             className="p-0"
+            
+
           >
             <CardBody className="p-0">
               {" "}
@@ -46,15 +47,16 @@ export default function Info() {
                 alt={item.title}
                 className="w-full h-full object-cover"
                 src={item.img}
+                
               />
             </CardBody>
-            <CardFooter className="text-small justify-between">
-              <b>{item.title}</b>
-              <p className="text-default-500">{item.price}</p>
-            </CardFooter>
+            
           </Card>
         ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
