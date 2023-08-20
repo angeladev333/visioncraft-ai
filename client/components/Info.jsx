487a791d24e660085600eb8903961e9e614eba85
Lucy Qi <l8qi@uwaterloo.ca>
2023-08-19 21:51:38 -0400

    making second card

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -18,9 +18,9 @@ export default function Info() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+    <div className="flex flex-col items-center justify-center min-h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       <div className="text-center z-10">
-      <div className="bg-white p-4 rounded shadow-md rounded-3xl">
+      <div className="bg-white p-4 rounded shadow-md rounded-3xl pb-20">
         <h2 className="text-4xl font-bold p-6 text-violet-500">BUILD</h2>
 
         <h1 className="text-5xl w-2/3 break-normal mx-auto mb-10 p-6 font-extrabold">
@@ -28,7 +28,7 @@ export default function Info() {
         </h1>
       
       
-      <div className="gap-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <div className="px-60 gap-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {list.map((item, index) => (
           <Card
             shadow="sm"
@@ -50,12 +50,59 @@ export default function Info() {
                 
               />
             </CardBody>
-            
+            {/* CardFooter content */}
           </Card>
         ))}
+
+        
           </div>
         </div>
+
+        
+
+
+        <div className="mt-8 bg-white p-4 rounded shadow-md rounded-3xl pb-20">
+        <h2 className="text-4xl font-bold p-6 text-violet-500">TRANSFORM</h2>
+
+        <h1 className="text-5xl w-2/3 break-normal mx-auto mb-10 p-6 font-extrabold">
+        Turn Your Visions into Reality
+        </h1>
+
+
+
+        <div className="flex-1">
+            <Image
+              width={300} 
+              height={300} 
+              alt="Imagination"
+              className="w-full h-full object-cover pl-10" 
+              src="/imagination.jpg"
+            />
+          </div>
+
+
+        <div className="flex-1">
+           
+            <p className="text-3xl text-gray-900 dark:text-white">
+              Use your desires to craft personalized creations with step-by-step guidance
+            </p>
+          </div>
+        
+
+
+
+
+        </div>
+
+
+
+
+      
+
       </div>
+
+     
+
     </div>
   );
 }
